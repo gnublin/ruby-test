92c100277910f06b11a2762560d1652f3f50f09b
Gauthier FRANÇOIS <gauthier.francois@resilience.care>
2023-09-27 15:48:01 +0200

    PG

--- run.rb
+++ run.rb
@@ -5,11 +5,23 @@ require 'active_support/all'
 require 'pg'
 require 'sinatra'
 require 'sinatra/base'
+require 'yaml'
+
 
 Bundler.require :default, :development
 
 class RubyTest < Sinatra::Application
     get '/' do
     'Hello world!'
+    end
+
+    get '/pg-ping' do
+      conn = PG.connect()
+      res = []
+      a = conn.exec( "SELECT * FROM pg_stat_activity" )
+      a.each_row do |row|
+        res << row
+      end
+      res.to_yaml
     end
 end
