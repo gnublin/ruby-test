8e90f82e32d8313e5e9bc6367482b9d6cc5eb202
Gauthier FRANCOIS <gauthier@openux.org>
2023-12-22 18:35:30 +0100

    Update run.rb

--- run.rb
+++ run.rb
@@ -10,25 +10,24 @@ require 'yaml'
 
 Bundler.require :default, :development
 
-exit 1
 
-ENV['PGHOST'] = ENV.fetch('POSTGRESQL_ADDON_HOST', '')
-ENV['PGPASSWORD']= ENV.fetch('POSTGRESQL_ADDON_PASSWORD', '')
-ENV['PGUSER'] = ENV.fetch('POSTGRESQL_ADDON_USER', '')
-ENV['PGDATABASE'] = ENV.fetch('POSTGRESQL_ADDON_DB', '')
+# ENV['PGHOST'] = ENV.fetch('POSTGRESQL_ADDON_HOST', '')
+# ENV['PGPASSWORD']= ENV.fetch('POSTGRESQL_ADDON_PASSWORD', '')
+# ENV['PGUSER'] = ENV.fetch('POSTGRESQL_ADDON_USER', '')
+# ENV['PGDATABASE'] = ENV.fetch('POSTGRESQL_ADDON_DB', '')
 
 class RubyTest < Sinatra::Application
     get '/' do
     'Hello world!'
     end
 
-    get '/pg-ping' do
-      conn = PG.connect()
-      res = []
-      a = conn.exec( "SELECT 1;" )
-      a.each_row do |row|
-        res << row
-      end
-      res.to_yaml
-    end
+    # get '/pg-ping' do
+    #   conn = PG.connect()
+    #   res = []
+    #   a = conn.exec( "SELECT 1;" )
+    #   a.each_row do |row|
+    #     res << row
+    #   end
+    #   res.to_yaml
+    # end
 end
